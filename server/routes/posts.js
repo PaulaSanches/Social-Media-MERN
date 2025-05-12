4b088590b9f1eb53bfe30ad47b1a77ce16828f7a
Paula Sanches <paula.correabarrientos@gmail.com>
2025-05-13 11:47:37 +1200

    Connecting to the Database and Routing

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
+const router = express.Router();
 
+//localhost:5000/posts
+// This is a test route to check if the posts route is working
+
+router.get('/', (req, res) => {
+  res.send('Posts route is working!');
+});
+
+export default router;
