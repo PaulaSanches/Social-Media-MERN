8364b500fd301a72922e849fab19f2edac629cf0
Paula Sanches <paula.correabarrientos@gmail.com>
2025-05-13 11:58:08 +1200

    controller file created

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/posts.js';    
+
 const router = express.Router();
 
 //localhost:5000/posts
 // This is a test route to check if the posts route is working
 
-router.get('/', (req, res) => {
-  res.send('Posts route is working!');
-});
+router.get('/', getPosts); // This is a test route to check if the posts route is working
 
 export default router;
