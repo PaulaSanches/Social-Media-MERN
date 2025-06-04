8244768583d34ce99db6a125638cb0b92849fde7
Paula Sanches <paula.correabarrientos@gmail.com>
2025-06-04 12:41:56 +1200

    reducers updated.

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,10 @@
-const reducer = (state = [], action) => {
+export default (posts = [], action) => {
   switch (action.type) {
-    case 'ADD_POST':
-      return [...state, action.payload];
-    case 'DELETE_POST':
-      return state.filter((post) => post.id !== action.payload);
-    case 'UPDATE_POST':
-      return state.map((post) =>
-        post.id === action.payload.id ? { ...post, ...action.payload } : post
-      );
+    case 'FETCH_ALL':
+      return posts;
+    case 'CREATE':
+      return posts;
     default:
-      return state;
+      return posts;
   }
 }
