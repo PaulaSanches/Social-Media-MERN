2d7a077c286bf64d2d16f018e0aac39cb36581be
Paula Sanches <paula.correabarrientos@gmail.com>
2025-06-16 17:36:23 +1200

    Hashtag fix

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -49,7 +49,7 @@ const Form = ({currentId, setcurrentId}) => {
                     <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator}onChange={(e) => setPostData({ ...postData, creator: e.target.value })}></TextField>
                     <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}onChange={(e) => setPostData({ ...postData, title: e.target.value })}></TextField>
                     <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message}onChange={(e) => setPostData({ ...postData, message: e.target.value })}></TextField>
-                    <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value })}></TextField>
+                    <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}></TextField>
                     <div className={classes.fileInput}><FileBase64 type="file" multiple={false}onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}/></div>
                     <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth value>Submit</Button>
                     <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth value>Clear</Button>
